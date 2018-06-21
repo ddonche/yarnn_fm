42b318be7e5b532d8f480aa746a0cc123026715a
ddonche <dan@fatalfitness.com>
2018-06-21 01:12:48 +0000

    finalized user follows

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,8 +4,6 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    Notification.create!(recipient_id: @user.id, 
-                      notified_by_id: current_user.id, notification_type: "follow")
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -15,8 +13,6 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow(@user)
-    Notification.create!(recipient_id: @user.id, 
-                      notified_by_id: current_user.id, notification_type: "unfollow")
     respond_to do |format|
       format.html { redirect_to @user }
       format.js

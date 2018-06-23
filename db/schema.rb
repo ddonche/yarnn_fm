dc04d9eda8fc2d4eeebcf75ad168a3414c3a783e
ddonche <dan@fatalfitness.com>
2018-06-23 21:37:53 +0000

    devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623155826) do
+ActiveRecord::Schema.define(version: 20180623212203) do
 
   create_table "albums", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -135,6 +135,10 @@ ActiveRecord::Schema.define(version: 20180623155826) do
     t.integer  "user_type",                         default: 0
     t.string   "slug"
     t.string   "bio"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["slug"], name: "index_users_on_slug", unique: true

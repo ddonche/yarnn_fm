9b9ed0d52d5acb476f5f3f8d2bcb312ba6b4d22f
ddonche <dan@fatalfitness.com>
2018-06-24 05:05:06 +0000

    sendgrid configuration

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,16 @@
 # Load the Rails application.
 require_relative 'application'
+require 'carrierwave/orm/activerecord'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
-require 'carrierwave/orm/activerecord'
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_starttls_auto => true
+}

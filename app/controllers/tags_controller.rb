0b5f51151ba35f86b335a20a596c35ca80f08f62
ddonche <dan@fatalfitness.com>
2018-06-26 18:25:59 +0000

    added tag show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,8 +5,8 @@ class TagsController < ApplicationController
   end
   
   def show
-    @albums = Album.tagged_with(params[:tag]).order('created_at DESC')
-    @tag_count = Album.tagged_with(params[:tag]).count
+    @tracks = Track.tagged_with(params[:tag]).order('created_at DESC').paginate(:page => params[:page], :per_page => 24)
+    @tag_count = Track.tagged_with(params[:tag]).count
     @tag = ActsAsTaggableOn::Tag.name
     def current_url
       url_for :only_path

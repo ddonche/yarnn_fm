ff2acafe7a77963de6412b8bb1d0fa1d29747752
ddonche <dan@fatalfitness.com>
2018-06-26 00:59:47 +0000

    getting album playlist to work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,15 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( amplitude.js )
 Rails.application.config.assets.precompile += %w( styles.css )
+Rails.application.config.assets.precompile += %w( player/jquery.mCustomScrollbar.css )
+Rails.application.config.assets.precompile += %w( player/artwork5.css )
+Rails.application.config.assets.precompile += %w( player/jquery-3.2.1.min.js )
+Rails.application.config.assets.precompile += %w( player/jquery.easing.1.3.js )
+Rails.application.config.assets.precompile += %w( player/jquery-ui.min.js )
+Rails.application.config.assets.precompile += %w( player/jquery.ui.touch-punch.min.js )
+Rails.application.config.assets.precompile += %w( player/jquery.mCustomScrollbar.concat.min.js )
+Rails.application.config.assets.precompile += %w( player/jsmediatags.min.js )
+Rails.application.config.assets.precompile += %w( player/hls.min.js )
+Rails.application.config.assets.precompile += %w( player/new.js )
+

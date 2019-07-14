beff975c33b1a65c1823ed5fb8c9b84e7a1c9c2f
ddonche <dan@fatalfitness.com>
2019-07-14 00:17:08 +0000

    stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_LeIn3WieNFTeZuiAdjnEYq3H',
-  :secret_key      => 'sk_test_3JD3nWr5J5R630SXI8hfHBhG'
+  :publishable_key => ENV[ 'STRIPE_PUBLISHABLE_KEY' ],
+  :secret_key      => ENV[ 'STRIPE_SECRET_KEY' ]
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]

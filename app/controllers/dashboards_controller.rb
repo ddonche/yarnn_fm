ca624466044cb1b977af00f16764a541a2725889
ddonche <dan@fatalfitness.com>
2019-07-15 04:09:21 +0000

    dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,12 +5,16 @@ class DashboardsController < ApplicationController
   def index 
     @pseudonyms = @user.pseudonyms.order('created_at DESC')
     @page_title = @user.username
-    @tracks = @user.tracks.order('created_at DESC').paginate(:page => params[:page], :per_page => 24)
+    @tracks = @user.tracks.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
+    @albums = @user.albums.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
+    @topics = @user.topics.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
+    @blogs = @user.blogs.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
+    @listings = @user.listings.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
     @users = @user.followers
     @followed_users = @user.following
     
     @activity = current_user.activities.build
-    @feed_items = current_user.feed.order("created_at DESC").paginate(:page => params[:page], :per_page => 24)
+    @feed_items = current_user.feed.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
     respond_to do |format| 
       format.html
       format.js

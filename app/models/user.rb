6793f2441b1192bd894ecf1126aadc51a0aa761b
ddonche <dan@fatalfitness.com>
2019-07-17 15:16:03 +0000

    google

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,29 +46,29 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed 
   has_many :followers, through: :passive_relationships, source: :follower
   
-  #def self.from_omniauth(auth)
-    #where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      #user.email = auth.info.email
-      #user.password = Devise.friendly_token[0, 20]
-      #user.name = auth.info.name   # assuming the user model has a name
-      #user.image = auth.info.image # assuming the user model has an image
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       #user.skip_confirmation!
-    #end
-  #end
-  
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
-
-    # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-        user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20])
     end
   end
+  
+  #def self.from_omniauth(access_token)
+  #  data = access_token.info
+  #  user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+  #  unless user
+  #      user = User.create(name: data['name'],
+  #         email: data['email'],
+  #         password: Devise.friendly_token[0,20])
+  #  end
+  #end
   
   # Follows a user.
   def follow(other_user)

d896ec961421a5f4ba345b5dd3b275a0c015ac05
ddonche <dan@fatalfitness.com>
2019-07-17 14:12:51 +0000

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190716204759) do
+ActiveRecord::Schema.define(version: 20190717140011) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -259,6 +259,8 @@ ActiveRecord::Schema.define(version: 20190716204759) do
     t.string   "website"
     t.integer  "favorited_count",                   default: 0
     t.string   "provider"
+    t.string   "google_token"
+    t.string   "google_refresh_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

822f988e30afe93d596b867e42c2f0557d39a33d
ddonche <dan@fatalfitness.com>
2019-07-17 14:37:08 +0000

    google oauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -13,21 +13,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
   
-  def googleAuth
-    # Get access tokens from the google server
-    access_token = request.env["omniauth.auth"]
-    user = User.from_omniauth(access_token)
-    log_in(user)
-    
-    # Access_token is used to authenticate request made from the rails application to the google server
-    user.google_token = access_token.credentials.token
-    
-    # Refresh_token to request new access_token
-    # Note: Refresh_token is only sent once during the first request
-    refresh_token = access_token.credentials.refresh_token
-    user.google_refresh_token = refresh_token if refresh_token.present?
-    user.save
-    redirect_to root_path
+  def google_oauth2
+      # You need to implement the method below in your model (e.g. app/models/user.rb)
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+
+      if @user.persisted?
+        flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+        sign_in_and_redirect @user, event: :authentication
+      else
+        session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      end
   end
 
   def stripe_connect

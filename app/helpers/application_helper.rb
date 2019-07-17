54e3f2692fb65cae2c66f14371dd6e116ed7f1ac
ddonche <dan@fatalfitness.com>
2019-07-17 04:50:36 +0000

    stripe stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def stripe_express_path
-    "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_DBKdNNndfDs8uWoBL6SOHhnwocLif9BV&scope=read_write"
+    "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=ca_DBKdHaMG5Mv9P18R4p7BvO6FQtvW80NI&scope=read_write"
   end
 end

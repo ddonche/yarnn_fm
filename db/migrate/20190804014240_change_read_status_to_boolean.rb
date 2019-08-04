b2ddb787bb5e4683378b8a5d5eefc03366fbcdad
ddonche <dan@fatalfitness.com>
2019-08-04 02:15:41 +0000

    trying to fix pg error

--- db/migrate/20190804014240_change_read_status_to_boolean.rb
+++ db/migrate/20190804014240_change_read_status_to_boolean.rb
@@ -1,5 +1,6 @@
 class ChangeReadStatusToBoolean < ActiveRecord::Migration[6.1]
   def change
     change_column :notifications, :read_status, :boolean
+    change_column :notifications, :read_status, 'boolean USING CAST(read_status AS boolean)'
   end
 end

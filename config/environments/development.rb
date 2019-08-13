0a0907e8770de43a80e999f1e19f8dde8512f5d5
Alex Mamonchik <alex.mamonchik@gmail.com>
2019-08-13 15:09:49 +0600

    added local host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
 
   #needed for Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
   #needed for c9
   config.hosts << "1d8ab0ac98db416d8e13b553f82de386.vfs.cloud9.us-east-1.amazonaws.com"
+  config.hosts << "yarn.local"
 end

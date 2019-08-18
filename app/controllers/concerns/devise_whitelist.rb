a9ec3983511a3301c044727f5a6510ed452d6915
ddonche <dan@fatalfitness.com>
2019-08-18 23:49:56 +0000

    stripe subscriptions

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -8,7 +8,8 @@ module DeviseWhitelist
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email, :password, :password_confirmation, :publisher_type])
     devise_parameter_sanitizer.permit(:account_update, keys: [
-      :username, :email, :password, :password_confirmation, :name, :image, :bio, :uid, :stripe_id, :publisher_type, :user_type
+      :username, :email, :password, :password_confirmation, :name, :image, :bio, :uid, :stripe_id, :publisher_type, :user_type,
+      :card_exp_month, :card_exp_year, :card_last4, :card_type
     ])
   end
 end
--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,17 +1,51 @@
 class SubscriptionsController < ApplicationController
-  before_action :authenticate_user!, except: [:new]
+  before_action :authenticate_user!, except: [:new, :create]
   before_action :redirect_to_signup, only: [:new]
   
-  def show
-  end
-  
   def new
+    if user_signed_in? && current_user.stripe_subscription_id?
+      redirect_to root_path, notice: "You are already a subscriber"
+    end
   end
   
   def create
-  end 
-  
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
+    plan_id = "plan_FdaxciAS6kvV5j"
+    plan = Stripe::Plan.retrieve(plan_id)
+    token = params[:stripeToken]
+
+    customer = if current_user.stripe_id?
+                Stripe::Customer.retrieve(current_user.stripe_id)
+               else
+                Stripe::Customer.create(email: current_user.email, source: token)
+              end
+
+    subscription = customer.subscriptions.create(plan: plan.id)
+
+    options = {
+      stripe_id: customer.id,
+      stripe_subscription_id: subscription.id,
+    }
+
+    options.merge!(
+      card_last4: params[:user][:card_last4],
+      card_exp_month: params[:user][:card_exp_month],
+      card_exp_year: params[:user][:card_exp_year],
+      card_type: params[:user][:card_type]
+    ) if params[:user][:card_last4]
+
+    current_user.update(options)
+
+    redirect_to root_path, notice: "Your subscription was setup successfully!"
+  end
+
   def destroy
+    customer = Stripe::Customer.retrieve(current_user.stripe_id)
+    customer.subscriptions.retrieve(current_user.stripe_subscription_id).delete
+    current_user.update(stripe_subscription_id: nil)
+
+    redirect_to root_path, notice: "Your subscription has been cancelled."
   end
   
   private

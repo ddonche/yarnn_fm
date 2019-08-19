3c6a39e7dbfa68c14eb4503674c5986006172699
ddonche <dan@fatalfitness.com>
2019-08-19 15:02:35 +0000

    notifications working again

--- app/models/track.rb
+++ app/models/track.rb
@@ -17,7 +17,7 @@ class Track < ApplicationRecord
 
   mount_uploader :image, ImageUploader
   mount_uploader :audio, AudioUploader
-  validates :audio, file_size: { less_than: audio_file_size_limit }, presence: true
+  validates :audio, file_size: { less_than: 15.megabytes }, presence: true
   validates :image, file_size: { less_than: 1.megabytes }, presence: true
   validates :tag_list, presence: true
 
@@ -30,14 +30,5 @@ class Track < ApplicationRecord
 
   def self.tagged_with(name)
     Tag.find_by_name!(name).tracks
-  end
-    
-
-  private
-
-  def audio_file_size_limit
-    return 15.megabytes unless user.stripe_subscription_id?
-
-    30.megabytes
   end
 end

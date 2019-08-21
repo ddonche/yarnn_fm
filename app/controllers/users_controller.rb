5afad0ded8b9194fc852b5d17e3abfe16f88a85e
ddonche <dan@fatalfitness.com>
2019-08-21 15:11:12 +0000

    cleaned assets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
   private
   
   def set_user
-    @user  = User.friendly.find(params[:id])
+    @user = User.friendly.find(params[:id])
   end
   
 end

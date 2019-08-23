8fa65188bf12b57323c86a4e58d27fc9277cfa5e
ddonche <dan@fatalfitness.com>
2019-08-23 05:27:50 +0000

    fixing reviews form

--- app/models/flag.rb
+++ app/models/flag.rb
@@ -1,6 +1,7 @@
 class Flag < ApplicationRecord
     enum flaggable_type: { track: 0, album: 1, listing: 2, topic: 3, blog: 4, review: 5 }
-    enum flag_type:      { abuse: 0, spam: 1, explicit: 2, file: 3, virus: 4, standards: 5, copyright: 6 }
+    enum flag_type:      { abuse: 0, spam: 1, explicit: 2, file: 3, virus: 4, standards: 5, 
+                            copyright: 6, unconsumed: 7 }
     
     belongs_to :flagged_by, class_name: 'User'
     belongs_to :flaggable, polymorphic: true

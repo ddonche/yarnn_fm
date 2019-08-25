fff3d8cee9c811c911bb202b20da7d4ed2f42a83
ddonche <dan@fatalfitness.com>
2019-08-25 03:23:15 +0000

    changing some form stylings

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,8 @@
 class Notification < ApplicationRecord
-  enum notifiable_type: { comment: 0, review: 1, message: 2, follow: 3, unfollow: 4, favorite: 5, 
-                            purchase: 6, unfavorite: 7, notation: 8 }
+  enum notifiable_type_old: { comment: 0, review: 1, message: 2, follow: 3, unfollow: 4, favorite: 5, 
+                            purchase: 6, unfavorite: 7, notation: 8, like: 9, dislike: 10 }
   enum read_status: { read: true, unread: false }
-  enum commentable_type: { Blog: 0, Track: 1, Topic: 2 }
+  enum commentable_type_old: { Blog: 0, Track: 1, Topic: 2 }
   belongs_to :notified_by, class_name: 'User'
   belongs_to :notifiable, polymorphic: true
 end
